ad7f12740c413d4fe9a581df7c866b9c914ac7b8
Binlaniua <Binlaniua@gmail.com>
2017-01-19 23:28:23 +0800

    remove 3rd lib

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import UIConfig from './lib/config';
 
 //
 import UIBtn from './lib/button/button';
+import UIImageBtn from './lib/button/image.button';
 import UIIconBtn from './lib/button/icon.button';
 import UITimerBtn from './lib/button/timer.button';
 import UIButtonGroup from './lib/button.group';
@@ -19,10 +20,8 @@ import UIScene from './lib/scene/';
 import UIScrollScene from './lib/scene/index.scroll';
 import UIHeader from './lib/scene/header';
 import UIBody from './lib/scene/body';
-import UIActionSheet from './lib/actionsheet';
 
 //
-import UIApp from './lib/app';
 import UIRoute from './lib/route';
 import UIKit from './lib/kit';
 import UIFetch from './lib/fetch';
@@ -39,9 +38,6 @@ import UITabBar from './lib/tabbar/';
 import UITabBarScene from './lib/scene.tabbar';
 
 //
-import UICarousel from './lib/carousel';
-
-//
 import UIScrollView from './lib/scrollview/advance.scroll.view';
 import UIRefreshControl from './lib/scrollview/refresh.control';
 
@@ -51,20 +47,24 @@ import UIWebView from './lib/webview';
 import UICascade from './lib/cascade';
 import UISearch from './lib/search';
 
+//
+import UIStyle from './lib/style';
+
 export {
     UIConfig,
+    UIStyle,
 
     UIWebView,
 
     UICascade,
     UISearch,
 
-    UIApp,
     UIRoute,
     UIKit,
     UIFetch,
 
     UIBtn,
+    UIImageBtn,
     UIIconBtn,
     UITimerBtn,
     UIButtonGroup,
@@ -76,11 +76,9 @@ export {
     UIScrollScene,
     UIHeader,
     UIBody,
-    UIActionSheet,
 
     UIForm,
 
-    UICarousel,
     UIProgress,
 
     UIListView,
