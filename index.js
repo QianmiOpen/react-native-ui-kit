741777e561581af60c52b716faad8de508b51f27
Binlaniua <Binlaniua@gmail.com>
2017-01-19 23:43:58 +0800

    remove module

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ import UIBtn from './lib/button/button';
 import UIImageBtn from './lib/button/image.button';
 import UIIconBtn from './lib/button/icon.button';
 import UITimerBtn from './lib/button/timer.button';
-import UIButtonGroup from './lib/button.group';
 
 //
 import UIScene from './lib/scene/';
-import UIScrollScene from './lib/scene/index.scroll';
 import UIHeader from './lib/scene/header';
 import UIBody from './lib/scene/body';
 
@@ -25,9 +23,6 @@ import UIBody from './lib/scene/body';
 import UIRoute from './lib/route';
 import UIKit from './lib/kit';
 import UIFetch from './lib/fetch';
-
-//
-import UIForm from './lib/form';
 
 //
 import UIListView from './lib/list.view/list';
@@ -38,26 +33,11 @@ import UITabBar from './lib/tabbar/';
 import UITabBarScene from './lib/scene.tabbar';
 
 //
-import UIScrollView from './lib/scrollview/advance.scroll.view';
-import UIRefreshControl from './lib/scrollview/refresh.control';
-
-//
-import UIProgress from './lib/progress';
-import UIWebView from './lib/webview';
-import UICascade from './lib/cascade';
-import UISearch from './lib/search';
-
-//
 import UIStyle from './lib/style';
 
 export {
     UIConfig,
     UIStyle,
-
-    UIWebView,
-
-    UICascade,
-    UISearch,
 
     UIRoute,
     UIKit,
@@ -67,23 +47,14 @@ export {
     UIImageBtn,
     UIIconBtn,
     UITimerBtn,
-    UIButtonGroup,
 
     UITabBar,
     UITabBarScene,
 
     UIScene,
-    UIScrollScene,
     UIHeader,
     UIBody,
 
-    UIForm,
-
-    UIProgress,
-
     UIListView,
     UISimpleRow,
-
-    UIScrollView,
-    UIRefreshControl,
 }
